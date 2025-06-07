0879600b38c4602d4b3b2c5c78511ff21dc961ad
iancossa <ianrafaelelias@gmail.com>
2025-06-07 15:53:21 +0530

    Update app.js

--- fundamentals/app.js
+++ fundamentals/app.js
@@ -4,12 +4,17 @@ const PORT = 3000;
 
 const userRoute = require("./routes/User");
 
-server.use("")
+server.use(logger);
 
 
 server.listen(PORT, ()=>{
     console.log("Server running");
 });
+
+function logger(req,res,next){
+    console.log('Log')
+    next();
+}
 
 
 
